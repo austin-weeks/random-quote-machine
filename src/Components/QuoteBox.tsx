3168d0cc66e8c19daa8f60703bf284f689a5242b
Austin Weeks <168379071+austin-weeks@users.noreply.github.com>
2024-05-27 14:54:33 -0500

    fixed async quote fetching

--- src/Components/QuoteBox.tsx
+++ src/Components/QuoteBox.tsx
@@ -13,22 +13,6 @@ export interface Quote {
     author: string;
 }
 
-const quotesURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-
-// async function loadQuotes() {
-//     const response = await fetch(quotesURL);
-//     const json = await response.json();
-//     const quotes: Quote[] = json["quotes"];
-//     return quotes;
-// }
-function loadQuotes() {
-    fetch(quotesURL).then(response => response.json()).then(data => {
-        return data;
-    });
-}
-
-//let quotes: Quote[];
-
 function getRandomQuote(): Quote {
     return quotes[Math.round(Math.random() * quotes.length)];
 }
